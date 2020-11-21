c745c70ad89beb70a680a93f7a9c28250cb865d5
Aqib Ijaz <aqibijaz3@gmail.com>
2020-11-21 15:24:32 +0500

    Create header Component and Implement on Routing

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import './App.css';
 // Import Pages
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch >
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
